31e106e067f144232c4f473b6b1a39822bac8dfa
Fernando <fmvalencia@uninorte.edu.co>
2024-10-12 22:34:01 -0500

    feat: Se separo el widget de box color para reusarse en diferentes lados

--- lib/UI/widgets/habit_page/habit_frequency.dart
+++ lib/UI/widgets/habit_page/habit_frequency.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:habit_app/ui/widgets/box_color.dart';
 
 class HabitFrequencyWidget extends StatelessWidget {
   final bool isDaily;
@@ -45,36 +46,6 @@ class HabitFrequencyWidget extends StatelessWidget {
   }
 }
 
-//Widget para envolver la frecuencia en cajita de color
-class BoxDays extends StatelessWidget {
-  final Color categoryColor;
-  final String text;
-  
-  const BoxDays({
-    super.key,
-    required this.categoryColor,
-    required this.text
-  });
-  
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 4.0),
-            decoration: BoxDecoration(
-              color: categoryColor.withOpacity(0.1),  // Fondo suave basado en el color de la categoría
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Text(
-              text,
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: Theme.of(context).textTheme.bodyLarge?.fontWeight,
-                color: categoryColor,
-              ),
-            ),
-          );
-  }
-}
 
 
 
